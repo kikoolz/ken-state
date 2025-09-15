b1aa4a0467ff8253b7f3f380db10678a31df4a46
kikoolz <kenlubs45@gmail.com>
2025-09-15 10:18:04 +0400

    Add the header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Link from "next/link";
+import { FaSearch } from "react-icons/fa";
+
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link href="/">
+          <h1 className="text-2xl font-bold">
+            <span className="text-slate-500">Ken</span>
+            <span className="text-slate-800">State</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-md flex items-center">
+          <input
+            type="text"
+            placeholder="Search.."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
+        </form>
+        <ul className="flex gap-4">
+          <Link href="/">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link href="/about">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link href="/sign-in">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
