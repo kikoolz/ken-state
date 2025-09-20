f7f73f927e46e81e98ca0bd68ad0856197e71128
kikoolz <kenlubs45@gmail.com>
2025-09-21 01:09:06 +0400

    Complete upload listing images functionality

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "ken-state.firebaseapp.com",
+  projectId: "ken-state",
+  storageBucket: "ken-state.firebasestorage.app",
+  messagingSenderId: "363250945074",
+  appId: "1:363250945074:web:732bd84b0d5cb72e09c02b",
+  measurementId: "G-1C2PF9N4QW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
